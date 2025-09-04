25b379423c904e561b5321638bf132ca488c7350
Aditya Yadav <py8390457@gmail.com>
2025-09-04 20:05:01 +0530

    Add main.cpp for decimal to binary conversion

--- main.cpp
+++ main.cpp
@@ -1,34 +1,20 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
-#include <iostream>
+#include<iostream>
 using namespace std;
-int gcd (int a,int b){
-    while(a>0 && b>0){
-        if (a>b){
-            a=a%b;
-        }
-        else{
-            b=b%a;
-        }
-    }
-    if(a==0) return b;
-    return a;
-}
-int lcm (int a,int b){
-    int gcdb = gcd(a,b);
-    return (a*b)/gcdb;
+
+int dectobin(int decnum){
+    int ans=0,pow=1;
+    while(decnum>0){
+        int rem=decnum%2;
+         decnum=decnum/2;
+        ans=ans+(rem*pow);
+        pow=pow*10;}
+       return ans; 
     
 }
 
 int main(){
-    cout<<gcd(20,28)<<endl;
-    cout<<lcm(20,28)<<endl;
-}
+int decnum=50;
+cout<<dectobin(decnum)<<endl;
+return 0;
 
-    
+}
